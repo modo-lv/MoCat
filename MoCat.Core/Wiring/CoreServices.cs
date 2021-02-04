b5ec37a8ee877807c4df9e2d3aa3e9d7733e643d
Modo <modo@modo.lv>
2021-02-05 00:48:37 +0200

    Added DB interface.

--- MoCat.Core/Wiring/CoreServices.cs
+++ MoCat.Core/Wiring/CoreServices.cs
@@ -4,10 +4,9 @@ using Microsoft.Extensions.DependencyInjection;
 namespace MoCat.Core.Wiring {
   public static class CoreServices {
     public static void AddTo(IServiceCollection services) {
-      // Startup service
       services.AddSingleton<IStartup, Startup>();
-
       services.AddSingleton<IFileSystem, FileSystem>();
+      services.AddSingleton<IDb, Db>();
     }
   }
 }
---
+++ MoCat.Core/Wiring/Db.cs
@@ -0,0 +1,10 @@
+﻿using LiteDB;
+
+namespace MoCat.Core.Wiring {
+  /// <summary>
+  /// Pre-request DB access instance.
+  /// </summary>
+  public class Db : LiteDatabase, IDb {
+    public Db() : base(Running.DbPath) { }
+  }
+}
