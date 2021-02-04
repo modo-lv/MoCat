ccecade2e78aa28334d8c47d9630269d867a3e26
Modo <modo@modo.lv>
2021-02-04 23:28:46 +0200

    Init

---
+++ MoCat.Core/Db/CoreDbContext.cs
@@ -0,0 +1,14 @@
+﻿using LiteDB;
+
+namespace MoCat.Core.Db
+{
+  public class CoreDbContext
+  {
+    public LiteDatabase Db; 
+    
+    public CoreDbContext()
+    {
+      this.Db = new LiteDatabase(":memory:");
+    }
+  }
+}
---
+++ MoCat.Web/Program.cs
@@ -0,0 +1,14 @@
+namespace MoCat.Web
+{
+  public static class Program
+  {
+    public static void Main()
+    {
+      //CreateHostBuilder(args).Build().Run();
+    }
+
+    // private static IHostBuilder CreateHostBuilder(string[] args) =>
+    //     Host.CreateDefaultBuilder(args)
+    //         .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+  }
+}
