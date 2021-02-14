f1b5a5e02b564cdd8f18d9ba580a9f3231db600a
modo_lv <modo@modo.lv>
2021-02-14 15:20:01 +0200

    Added owner entity.

--- MoCat.Core/Entities/IDbContext.cs
+++ MoCat.Core/Entities/IDbContext.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using LiteDB;
 using MoCat.Core.Entities.Db;
+using MoCat.Core.Main;
 
 namespace MoCat.Core.Entities {
   public interface IDbContext {
@@ -8,8 +11,27 @@ namespace MoCat.Core.Entities {
     
     ILiteCollection<LedgerEntry> LedgerEntries { get; }
     
-    UserConfig UserConfig => this.Db.GetCollection<UserConfig>().Query().SingleOrDefault() ?? new UserConfig();
+    UserConfig UserConfig => 
+      this.Db.GetCollection<UserConfig>().Query().SingleOrDefault() ?? new UserConfig();
 
+    /// <summary>
+    /// All asset owners.
+    /// </summary>
+    IDictionary<String, Owner> Owners => 
+      this.Db.GetCollection<Owner>()
+        .FindAll()
+        .ToDictionary(_ => _.Id, _ => _);
+
+    /// <summary>
+    /// Active asset owners.
+    /// </summary>
+    IDictionary<String, Owner> ActiveOwners =>
+      this.Db.GetCollection<Owner>().Query()
+        .Where(_ => _.IsActive)
+        .ToEnumerable().ToDictionary(_ => _.Id, _ => _);
+
+    
+    
     void Save<T>(Func<T> data) {
       this.Db.GetCollection<T>().Upsert(data());
     }
---
+++ MoCat.Core/Main/Owner.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MoCat.Core.Main {
+  /// <summary>
+  /// Asset owner.
+  /// </summary>
+  public class Owner {
+    /// <summary>
+    /// Name of the owner, also used as ID.  
+    /// </summary>
+    public String Id { get; init; }
+    
+    /// <summary>
+    /// Inactive owners cannot be added to new transactions, but are still available in search, filtering etc.  
+    /// </summary>
+    public Boolean IsActive { get; set; }
+
+    /// <inheritdoc cref="Owner"/>
+    /// <param name="id"><see cref="Id"/></param>
+    /// <param name="isActive"><see cref="IsActive"/></param>
+    public Owner(String id, Boolean isActive = true) {
+      this.Id = id;
+      this.IsActive = isActive;
+    }
+  }
+}
